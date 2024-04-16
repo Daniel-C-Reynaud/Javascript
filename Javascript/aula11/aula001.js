5c04741c212033511599dcf4207d086a17b827cd
Daniel Capote Reynaud <danielcapotereynaud@gmail.com>
2024-04-16 14:11:14 -0300

    Initial commit

---
+++ Javascript/aula11/aula001.js
@@ -0,0 +1,9 @@
+var vel = 10
+
+var max_vel = 80
+
+if (vel > max_vel) {
+     console.log('multado');
+} else {
+     console.log('passar bem');
+}
---
+++ Javascript/aula13/script.js
@@ -0,0 +1,8 @@
+console.log('ola')
+
+let num = [5, 8, 3]
+
+num[3] = 1
+num.push(10)
+console.log(num)
+num.sort()
